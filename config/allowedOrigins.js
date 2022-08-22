4871fc23477460ab3d0b00fab9018866af16665f
Nick Dalby <nckdlby@gmail.com>
2022-08-22 15:14:33 +0200

    add dev server to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  'https://enigmatic-meadow-32236.herokuapp.com'
+  'https://enigmatic-meadow-32236.herokuapp.com',
+  'http://http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
