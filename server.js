b8026fe74751b64a59088098518ecf5bfa171cd6
Nick Dalby <nckdlby@gmail.com>
2022-08-22 11:28:27 +0200

    edit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(logger)
 app.use(credentials)
 
 // Cross Origin Resource Sharing
-app.use(cors())
+app.use(cors(corsOptions))
 
 // built-in middleware to handle urlencoded data
 app.use(express.urlencoded({ extended: false }))
