437b3453754e1c0eba701f3068b420eead9c700d
Nick Dalby <nckdlby@gmail.com>
2022-08-23 18:48:13 +0200

    url edit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['https://nick-dalby.github.io/', 'http://localhost:3000']
+const allowedOrigins = ['https://nick-dalby.github.io', 'http://localhost:3000']
 
 module.exports = allowedOrigins
